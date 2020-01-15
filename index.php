b9d301e6c53dbbabf8926202378f33275fb5d64e
Madan Malhotra <madan@themediabulletin.com>
2020-01-15 19:39:29 +0530

    added header and footer code

--- index.php
+++ index.php
@@ -4,103 +4,66 @@ get_header(); ?>
 
 
 	<!-- Feature post -->
-	<section class="bg0">
-		<div class="container">
-			<div class="row m-rl--1">
-				<div class="col-md-6 p-rl-1 p-b-2">
-					<div class="bg-img1 size-a-3 how1 pos-relative" style="background-image: url(<?php echo get_template_directory_uri(). '/images/post-01.jpg' ?>);">
-						<a href="blog-detail-01.html" class="dis-block how1-child1 trans-03"></a>
 
-						<div class="flex-col-e-s s-full p-rl-25 p-tb-20">
-							<a href="#" class="dis-block how1-child2 f1-s-2 cl0 bo-all-1 bocl0 hov-btn1 trans-03 p-rl-5 p-t-2">
-								Business
-							</a>
+  <section class="bg0">
+  			<div class="row m-rl-0 justify-content-center">
+  				<div class="col-md-4 p-rl-1 p-b-2">
+  					<div class="bg-img1 size-a-11 how1 pos-relative" style="background-image: url(<?php echo get_template_directory_uri().'/images/post-01.jpg' ?>);">
+  						<a href="blog-detail-02.html" class="dis-block how1-child1 trans-03"></a>
 
-							<h3 class="how1-child2 m-t-14 m-b-10">
-								<a href="blog-detail-01.html" class="how-txt1 size-a-6 f1-l-1 cl0 hov-cl10 trans-03">
-									Microsoft quisque at ipsum vel orci eleifend ultrices
-								</a>
-							</h3>
+  						<div class="flex-col-e-s s-full p-rl-25 p-tb-18">
+  							<a href="#" class="dis-block how1-child2 f1-s-2 cl0 bo-all-1 bocl0 hov-btn1 trans-03 p-rl-5 p-t-2">
+  								Technology
+  							</a>
 
-							<span class="how1-child2">
-								<span class="f1-s-4 cl11">
-									Jack Sims
-								</span>
+  							<h3 class="how1-child2 m-t-14 m-b-10">
+  								<a href="blog-detail-02.html" class="f1-l-1 cl0 hov-cl10 trans-03 respon1">
+  									Microsoft quisque at ipsum vel orci eleifend ultrices
+  								</a>
+  							</h3>
+  						</div>
+  					</div>
+  				</div>
 
-								<span class="f1-s-3 cl11 m-rl-3">
-									-
-								</span>
+  				<div class="col-md-4 p-rl-1 p-b-2">
+  					<div class="bg-img1 size-a-11 how1 pos-relative" style="background-image: url(<?php echo get_template_directory_uri().'/images/post-19.jpg' ?>);">
+  						<a href="blog-detail-02.html" class="dis-block how1-child1 trans-03"></a>
 
-								<span class="f1-s-3 cl11">
-									Feb 16
-								</span>
-							</span>
-						</div>
-					</div>
-				</div>
+  						<div class="flex-col-e-s s-full p-rl-25 p-tb-18">
+  							<a href="#" class="dis-block how1-child2 f1-s-2 cl0 bo-all-1 bocl0 hov-btn1 trans-03 p-rl-5 p-t-2">
+  								Technology
+  							</a>
 
-				<div class="col-md-6 p-rl-1">
-					<div class="row m-rl--1">
-						<div class="col-12 p-rl-1 p-b-2">
-							<div class="bg-img1 size-a-4 how1 pos-relative" style="background-image: url(<?php echo get_template_directory_uri(). '/images/post-02.jpg' ?>);">
-								<a href="blog-detail-01.html" class="dis-block how1-child1 trans-03"></a>
+  							<h3 class="how1-child2 m-t-14 m-b-10">
+  								<a href="blog-detail-02.html" class="f1-l-1 cl0 hov-cl10 trans-03 respon1">
+  									London ipsum dolor sit amet, consectetur adipiscing elit.
+  								</a>
+  							</h3>
+  						</div>
+  					</div>
+  				</div>
 
-								<div class="flex-col-e-s s-full p-rl-25 p-tb-24">
-									<a href="#" class="dis-block how1-child2 f1-s-2 cl0 bo-all-1 bocl0 hov-btn1 trans-03 p-rl-5 p-t-2">
-										Culture
-									</a>
+  				<div class="col-md-4 p-rl-1 p-b-2">
+  					<div class="bg-img1 size-a-11 how1 pos-relative" style="background-image: url(<?php echo get_template_directory_uri().'/images/post-20.jpg' ?>);">
+  						<a href="blog-detail-02.html" class="dis-block how1-child1 trans-03"></a>
 
-									<h3 class="how1-child2 m-t-14">
-										<a href="blog-detail-01.html" class="how-txt1 size-a-7 f1-l-2 cl0 hov-cl10 trans-03">
-											London ipsum dolor sit amet, consectetur adipiscing elit.
-										</a>
-									</h3>
-								</div>
-							</div>
-						</div>
+  						<div class="flex-col-e-s s-full p-rl-25 p-tb-18">
+  							<a href="#" class="dis-block how1-child2 f1-s-2 cl0 bo-all-1 bocl0 hov-btn1 trans-03 p-rl-5 p-t-2">
+  								Life Style
+  							</a>
 
-						<div class="col-sm-6 p-rl-1 p-b-2">
-							<div class="bg-img1 size-a-5 how1 pos-relative" style="background-image: url(<?php echo get_template_directory_uri(). '/images/post-03.jpg' ?>);">
-								<a href="blog-detail-01.html" class="dis-block how1-child1 trans-03"></a>
+  							<h3 class="how1-child2 m-t-14 m-b-10">
+  								<a href="blog-detail-02.html" class="f1-l-1 cl0 hov-cl10 trans-03 respon1">
+  									Pellentesque dui nibh, pellentesque ut dapibus ut
+  								</a>
+  							</h3>
+  						</div>
+  					</div>
+  				</div>
+  			</div>
+  	</section>
 
-								<div class="flex-col-e-s s-full p-rl-25 p-tb-20">
-									<a href="#" class="dis-block how1-child2 f1-s-2 cl0 bo-all-1 bocl0 hov-btn1 trans-03 p-rl-5 p-t-2">
-										Life Style
-									</a>
-
-									<h3 class="how1-child2 m-t-14">
-										<a href="blog-detail-01.html" class="how-txt1 size-h-1 f1-m-1 cl0 hov-cl10 trans-03">
-											Pellentesque dui nibh, pellen-tesque ut dapibus ut
-										</a>
-									</h3>
-								</div>
-							</div>
-						</div>
-
-						<div class="col-sm-6 p-rl-1 p-b-2">
-							<div class="bg-img1 size-a-5 how1 pos-relative" style="background-image: url(<?php echo get_template_directory_uri(). '/images/post-04.jpg' ?>);">
-								<a href="blog-detail-01.html" class="dis-block how1-child1 trans-03"></a>
-
-								<div class="flex-col-e-s s-full p-rl-25 p-tb-20">
-									<a href="#" class="dis-block how1-child2 f1-s-2 cl0 bo-all-1 bocl0 hov-btn1 trans-03 p-rl-5 p-t-2">
-										Sport
-									</a>
-
-									<h3 class="how1-child2 m-t-14">
-										<a href="blog-detail-01.html" class="how-txt1 size-h-1 f1-m-1 cl0 hov-cl10 trans-03">
-											Motobike Vestibulum vene-natis purus nec nibh volutpat
-										</a>
-									</h3>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-
-	<!-- Post -->
+		<!-- Post -->
 	<section class="bg0 p-t-70">
 		<div class="container">
 			<div class="row justify-content-center">
